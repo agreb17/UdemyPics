ff2589d970f20be002783ba4624135d0671b3864
Anthony Greb <agreb255@gmail.com>
2019-04-16 17:35:10 -0400

    added a bit of styling for the search bar

--- pics/src/components/App.js
+++ pics/src/components/App.js
@@ -3,7 +3,9 @@ import SearchBar from "./SearchBar"
 
 const App = () => {
     return (
+        <div className="ui container" style={{marginTop: '10px'}}>
         <SearchBar />
+        </div>
     )
     
 };
