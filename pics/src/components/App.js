cd8ad1683067c7748ece8c7b560513cc99c06bcb
Anthony Greb <agreb255@gmail.com>
2019-04-17 18:42:19 -0400

    added async await for get request

--- pics/src/components/App.js
+++ pics/src/components/App.js
@@ -4,18 +4,21 @@ import SearchBar from "./SearchBar"
 
 
 class App extends React.Component {
-    onSearchSubmit(term) {
-        axios.get('https://api.unsplash.com/search/photos', {
+    state = { images: [] };
+    onSearchSubmit = async (term) => {
+        const response = await axios.get('https://api.unsplash.com/search/photos', {
             params: {query: term},
             headers: {
                 Authorization: 'Client-ID 222fdb96124f14060bab7e14ab5f04cd2219cd8c581e1824e4ce0db7e8d7a5b4'
             }
         });
+        this.setState({images: response.data.results});
     }
     render() {
         return (
             <div className="ui container" style={{marginTop: '10px'}}>
             <SearchBar onSubmit={this.onSearchSubmit}/>
+            Found: {this.state.images.length} images.
             </div>
         )
     }
