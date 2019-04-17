c30aeadbc3656c02d7e724c6fb31ac81baeae881
Anthony Greb <agreb255@gmail.com>
2019-04-17 16:48:55 -0400

    set up axios request to retrieve pics

--- pics/src/components/App.js
+++ pics/src/components/App.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import axios from 'axios';
 import SearchBar from "./SearchBar"
+
 
 class App extends React.Component {
     onSearchSubmit(term) {
-        console.log(term);
+        axios.get('https://api.unsplash.com/search/photos', {
+            params: {query: term},
+            headers: {
+                Authorization: 'Client-ID 222fdb96124f14060bab7e14ab5f04cd2219cd8c581e1824e4ce0db7e8d7a5b4'
+            }
+        });
     }
     render() {
         return (
