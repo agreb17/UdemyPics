9f53f83d632fee56593866813cc12f714158a379
Anthony Greb <agreb255@gmail.com>
2019-04-18 18:34:48 -0400

    added alt, key, and destructured image prop

--- pics/src/components/ImageList.js
+++ pics/src/components/ImageList.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 
 const ImageList = props => {
-    const images = props.images.map(image => {
-        return <img src={image.urls.regular} alt={''}/>;
+    const images = props.images.map(({description, id, urls }) => {
+        return <img src={urls.regular} alt={description} key={id}/>;
     });
     return <div>{images}</div>
 }
